3fccaf087b24daaf76c2bf337f2e02140e6688ec
chrisachiles0 <56chris10@gmail.com>
2021-04-22 19:45:48 -0700

    Initial implementation of interface for database functionality

---
+++ AgrecomDB/DBInterface.swift
@@ -0,0 +1,17 @@
+//
+//  DBInterface.swift
+//  AgrecomApp
+//
+//  Created by Chris Achiles on 4/22/21.
+//
+
+//import the PostgresNIO library
+import PostgresNIO
+
+struct DBInterface: View {
+    //
+}
+
+struct DBInterface_Previews: PreviewProvider {
+    //
+}
